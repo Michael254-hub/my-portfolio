dacaf76e73ee308485eaec5d51a1625c7d88b227
Michael254-hub <michaelsimiyu54@gmail.com>
2025-06-14 23:37:57 +0300

    other updates

--- script.js
+++
@@ -1,8 +0,0 @@
-const book = {
-            title: "The Great Gatsby",
-            color: "blue",
-            size: "medium",
-            type: "fiction",
-        }
-        const { title, color, size, type } = book;
-        document.getElementById("demo").innerHTML = `Book Title: ${title}, Color: ${color}, Size: ${size}, Type: ${type}`;
